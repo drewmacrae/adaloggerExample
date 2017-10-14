0d8c2ad16b6d5b247cc716414db9cbc20b2889b0
Drew Macrae <Drew@Linkitz.com>
2017-10-14 12:36:19 -0700

    Initial commit
    Small changes to examples from Adafruit.com
    -Wait for serial connection to be established so I can see what's going on over USB
    -Close file to write updates, otherwise they never seem to make it onto the card
    Don't know why it didn't work initially, maybe serial library has changed?

---
+++ adaloggerExample.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include <SD.h>
+
+// Set the pins used
+#define cardSelect 4
+
+char filename[15];
+File logfile;
+
+// blink out an error code
+void error(uint8_t errno) {
+  while(1) {
+    uint8_t i;
+    for (i=0; i<errno; i++) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
+    for (i=errno; i<10; i++) {
+      delay(200);
+    }
+  }
+}
+
+// This line is not needed if you have Adafruit SAMD board package 1.6.2+
+//   #define Serial SerialUSB
+
+void setup() {
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  while(!Serial){}
+  Serial.println("\r\nAnalog logger test");
+  pinMode(13, OUTPUT);
+
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(cardSelect)) {
+    Serial.println("Card init. failed!");
+    error(2);
+  }
+  strcpy(filename, "ANALOG00.TXT");
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = '0' + i/10;
+    filename[7] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if( ! logfile ) {
+    Serial.print("Couldnt create "); 
+    Serial.println(filename);
+    error(3);
+  }
+  Serial.print("Writing to "); 
+  Serial.println(filename);
+
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.println("Ready!");
+  logfile.close();
+  
+}
+
+uint8_t i=0;
+void loop() {
+  digitalWrite(8, HIGH);
+  logfile = SD.open(filename, FILE_WRITE);
+  logfile.print("A0 = "); logfile.println(analogRead(0));
+  logfile.close();
+  Serial.print("A0 = "); Serial.println(analogRead(0));
+  digitalWrite(8, LOW);
+  
+  delay(100);
+}
